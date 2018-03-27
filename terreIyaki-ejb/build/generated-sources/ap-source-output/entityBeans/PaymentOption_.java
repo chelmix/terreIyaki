89927c5f7ac393871476feae76d3d386348e736a
Unknown <chelmix@gmail.com>
2018-03-27 21:22:38 +0200

    Debug

--- terreIyaki-ejb/build/generated-sources/ap-source-output/entityBeans/PaymentOption_.java
+++ terreIyaki-ejb/build/generated-sources/ap-source-output/entityBeans/PaymentOption_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T17:19:15")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T21:20:41")
 @StaticMetamodel(PaymentOption.class)
 public class PaymentOption_ { 
 
