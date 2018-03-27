0a86274a3552d173d085513b32e09e25e4df170c
Unknown <chelmix@gmail.com>
2018-03-27 17:21:47 +0200

    Bugs correction

--- terreIyaki-ejb/build/generated-sources/ap-source-output/entityBeans/Payment_.java
+++ terreIyaki-ejb/build/generated-sources/ap-source-output/entityBeans/Payment_.java
@@ -8,15 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< refs/remotes/origin/master
-<<<<<<< refs/remotes/origin/master
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T14:33:23")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T14:11:32")
->>>>>>> Ajout changement Tables
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T14:08:06")
->>>>>>> Commit de cuisine
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T17:19:15")
 @StaticMetamodel(Payment.class)
 public class Payment_ { 
 
